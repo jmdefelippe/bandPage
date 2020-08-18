d34b46fe1386db4b35241dbd6518429e91d5e017
jmdefelippe <jmdefelippe@gmail.com>
2020-08-18 18:06:31 -0300

    Template página web para una banda: Boostrap

---
+++ js/Scripts.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+	$(document).ready(function(){
+		$('#resultado').hide();
+	});
+	$('#mostrar').mouseover(function() {
+	    $('#resultado').toggle();
+	});
+	$('#mostrar').mouseout(function() {
+	    $('#resultado').toggle();
+	});
+</script>
